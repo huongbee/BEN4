ed158cfc6a6e22db82048015dc6c758655552e1b
HuongNTN <huongntn@payme.vn>
2022-02-27 12:15:39 +0700

    update

--- Buoi2/a.js
+++ Buoi2/a.js
@@ -35,8 +35,37 @@
 // console.log(typeof NaN);
 // liet ke cac gia tri return false: false 0 '' null undefined NaN
 // || &&
-if (0 || 1) {
-  console.log('Menh de true');
-} else {
-  console.log('menh de false');
-}
+
+// if (0 || 1) {
+//   console.log('Menh de true');
+// } else {
+//   console.log('menh de false');
+// }
+
+// const a = 'X';
+// console.log("Gia tri cua a = ${a}");
+// console.log(`Gia tri cua a = ${a}`);
+
+// const a = 10;
+// console.log('A = ' + a);
+// let b = '400000000000000000000';
+// let c = '5';
+// console.log(b + c); // 3 ? 12
+// console.log(parseInt(b) + parseInt(c));
+// console.log(b - 0 + c - 0);
+// // console.log((b - 0) + (c - 0));
+// console.log(~~b + ~~c);
+// console.log(Number(b) + Number(c));
+// console.log(!!b + !!c);
+// console.log(eval(`${b}+${c}`));
+// console.log(eval('2+4/6+39+2*2'));
+
+// const a = 0.1;
+// const b = 0.2;
+// console.log(a + b == 0.3);
+const a = 0.2;
+const b = 0.7;
+console.log(a);
+console.log(b);
+console.log(a + b);
+console.log(a + b == 0.9);
