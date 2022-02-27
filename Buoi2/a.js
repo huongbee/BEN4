2fa3b603410208b5baa10b0215e9a4fa3956ae05
HuongNTN <huongntn@payme.vn>
2022-02-27 11:41:40 +0700

    init b2

---
+++ Buoi2/a.js
@@ -0,0 +1,42 @@
+// const obj = {
+//   name: 'foo',
+// };
+// obj.age = 12;
+// // console.log(obj);
+
+// // console.log(typeof obj);
+// // obj.push(4); // error
+
+// const arr = ['foo']; // 0
+// arr['name'] = 'bar'; // 1
+// arr.age = 12;
+// arr['key.a'] = 'A';
+// arr.push('teo');
+// console.log(arr);
+
+// console.log(typeof arr);
+// // arr.push(2)
+// // console.log(arr);
+// if (Array.isArray(arr)) {
+//   arr.push(3)
+// }
+// console.log(arr);
+
+// if (Array.isArray(obj)) {
+//   console.log('Obj is not array');
+// }
+// const bool = true;
+// const a = null;  // '' null undefined 0
+// console.log(typeof a);
+// const b = undefined;
+// console.log(typeof b);
+// // console.log(typeof 0);
+// const c = NaN;
+// console.log(typeof NaN);
+// liet ke cac gia tri return false: false 0 '' null undefined NaN
+// || &&
+if (0 || 1) {
+  console.log('Menh de true');
+} else {
+  console.log('menh de false');
+}
