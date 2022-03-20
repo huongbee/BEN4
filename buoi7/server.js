30fe36c864526a81c95b5ae82e755e9877ca3e0c
Huong Huong <huongnguyen08.cv@gmail.com>
2022-03-20 10:57:20 +0700

    Add files via upload

--- buoi7/server.js
+++ buoi7/server.js
@@ -10,7 +10,7 @@ app.get('/:pheptinh/:a/:b', (req, res) => {
     if (arrPheptinh.find(pt => pheptinh === pt)) {
       const cal = new Calculate(pheptinh, a, b)
       res.send({
-        pheptinh: cal.pt,
+        pheptinh: cal.operator,
         a: +a,
         b: +b,
         result: cal.result
@@ -23,4 +23,5 @@ app.get('/:pheptinh/:a/:b', (req, res) => {
     res.send({ error: true, message: error.message })
   }
 })
+app.get('/', (req, res) => { res.send({ message: 'Thanh cong' }) });
 app.listen(3000, () => console.log(`Server start on port 3000`))
