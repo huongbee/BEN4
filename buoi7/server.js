cdae833f64a81fa5d687e985971718f442ef2463
HuongNTN <huongntn@payme.vn>
2022-03-20 10:42:44 +0700

    add buoi7

---
+++ buoi7/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const Calculate = require('./lib/Calculate');
+
+app.get('/:pheptinh/:a/:b', (req, res) => {
+  try {
+    let { pheptinh, a, b } = req.params
+    pheptinh = pheptinh.toLowerCase()
+    arrPheptinh = ['cong', 'tru', 'nhan', 'chia', 'chiadu'];
+    if (arrPheptinh.find(pt => pheptinh === pt)) {
+      const cal = new Calculate(pheptinh, a, b)
+      res.send({
+        pheptinh: cal.pt,
+        a: +a,
+        b: +b,
+        result: cal.result
+      })
+    }
+    else {
+      res.send({ error: true, message: 'CANNOT FIND OPERATOR!' })
+    }
+  } catch (error) {
+    res.send({ error: true, message: error.message })
+  }
+})
+app.listen(3000, () => console.log(`Server start on port 3000`))
