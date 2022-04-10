ca3089f02418b64ee110394d95a41cac4e4ca0e4
HuongNTN <huongntn@payme.vn>
2022-04-10 11:38:30 +0700

    finr

--- buoi11/StudentModel.js
+++ buoi11/StudentModel.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Student = new Schema({
-  id: { type: Number, required: true, unique: true },
   name: { type: String },
   avatar: String,
-  email: { type: String, unique: true }
+  email: { type: String, unique: true },
+  // tags: [String], // ["AAAA", 'BBBB', '.....]
+  tags: [{
+    id: String,
+    value: String
+  }]  // [{id: 1, value: "AAAAA"},{ id: 2, value:"BBBBB"}]
 });
 const StudentModel = mongoose.model('Student', Student);
 Student.index({
-  id: { unique: true },
   email: { unique: true, sparse: true },
-  id: 1
 })
 module.exports = StudentModel;
-
-// 62524ca4fbf019f42fc97903 ObjectId hex: [09abcdef]
--- buoi11/index.js
+++ buoi11/index.js
@@ -3,7 +3,7 @@ const StudentModel = require('./StudentModel');
 // mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]
 mongoose.connect('mongodb://localhost:27017/my_database', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log("connect success");
+    // console.log("connect success");
   })
   .catch(err => console.log(err));
 
@@ -11,10 +11,68 @@ mongoose.connect('mongodb://localhost:27017/my_database', { useNewUrlParser: tru
 // student.id = 1;
 // student.name = 'Student Name';
 // student.save();
-StudentModel.create({ id: 1, name: 'Nguyen Van Ti', avatar: 'http://example.com/.png' })
+// StudentModel.create({ name: 'Nguyen Van Ti', avatar: 'http://example.com/.png' })
 
-StudentModel.insertMany([
-  // { id: 1, name: 'Nguyen Van Ti' },
-  { id: 2, name: 'Nguyen Van Teo', email: 'a@gmail.com' }
-])
-// update delete find
+// StudentModel.insertMany([
+//   { name: 'Nguyen Van A', email: 'a@gmail.com' },
+//   { name: 'Nguyen Van B', email: 'b@gmail.com' },
+//   { name: 'Nguyen Van C', email: 'c@gmail.com' },
+//   { name: 'Nguyen Van D', email: 'd@gmail.com' },
+//   { name: 'Nguyen Van E', email: 'e@gmail.com' },
+//   { name: 'Nguyen Van F', email: 'f@gmail.com' },
+//   { name: 'Nguyen Van G', email: 'g@gmail.com' }
+// ])
+// find
+// SELECT * FROM Student .find() || .find({})
+//  WHERE  (email = 'a@gmail.com' AND name LIKE '%A%') OR (1=1 OR 2=2)
+// OR
+// page 1 = 0,10
+// page 2 = 10,20
+//
+(async () => {
+  // const studentA = await StudentModel.find({
+  //   email: 'a@gmail.com'
+  // });
+  const studentA = await StudentModel.find({
+    // email: 'a@gmail.com',
+    // name: {
+    //   $regex: /Van A$/ // LIKE "%Van A"
+    // }
+    // name: {
+    //   $regex: /^Tran[a-zA-Z ]/ // LIKE 'Tran%'
+    // }
+    // name: {
+    //   $regex: /Van/ // LIKE 'Tran%'
+    // }
+    // $and: [
+    //   {
+    //     email: 'a@gmail.com', // 1
+    //     field: "AAAAA"
+    //   },
+    //   {
+    //     name: { // 9
+    //       $regex: /Van/
+    //     }
+    //   }
+    // ]
+    // $or: [
+    //   {
+    //     email: 'admin@gmail.com', // 1
+    //   },
+    //   {
+    //     name: { // 9
+    //       $regex: /Van/
+    //     }
+    //   }
+    // ]
+    // tags: { $regex: /tag/ } // [String],
+    'tags.value': 'tag1'
+  })
+  //.where('tags').size(1);
+  //.skip(2).limit(5)
+  // .limit(2);
+  console.log(studentA);
+})();
+// update
+
+// delete
