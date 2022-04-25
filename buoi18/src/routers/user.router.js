f036236d323f6d9f814e5be706995fb727f28ae1
HuongNTN <huongntn@payme.vn>
2022-04-25 20:55:55 +0700

    update

--- buoi18/src/routers/user.router.js
+++ buoi18/src/routers/user.router.js
@@ -28,5 +28,16 @@ router.post('/login', async (req, res, next) => {
   next();
   return res.redirect('/todo-list');
 });
+/**
+ * Login
+ * 1. khóa tk user nếu login sai password quá 5 lần: tạo them field lưu || redis || file
+ * 2. kiểm tra user có bị block
 
+ * Quên pass -> kiểm tra tk co bi khoa hay ko => gửi OTP(6 digits random) -> user nhập OTP -> kiểm tra tk co bi khoa hay ko,
+ *      => mess: Bạn đã nhập sai OTP quá 5 lần liên tiếp, vui lòng thử lại sau 30p20s hoặc liên hệ bộ phận CSKH để được hỗ trợ!
+ *   Gửi lại OTP -> ?s true|false -> không gửi OTP (mess: Vui lòng thử lại sau 30p15s)
+ *   Change pass: password === re passs ->
+ * Update pass: user logged in: không câp nhật lại giống 2 mk cũ
+ * cron job 00:00 scan table users (queue) => check lasted update pass => khóa tài khoản, push noti về client
+ */
 module.exports = router;
