1a3432470c6d4f2548796745ac14450f6227e2b9
HuongNTN <huongntn@payme.vn>
2022-05-09 23:33:21 +0700

    redis

--- buoi18/src/index.js
+++ buoi18/src/index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const userRouter = require('./routers/user.router');
 const todoListRouter = require('./routers/todo-list.router');
+const RedisService = require('./services/redis.service');
 app.set('view engine', 'ejs');
 app.set('views', 'src/views');
 app.use(express.static('src/public'));
@@ -25,3 +26,14 @@ app.use('/todo-list', todoListRouter);
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 })
+
+const redis = new RedisService();
+(async () => {
+  // const result = await redis.set('keyTest', JSON.stringify({ name: 'Kmin', age: 2 }));
+  // console.log(result);
+  // const result = await redis.del('keyTest');
+  // console.log(result);
+  // const result = await redis.get('keyTest');
+  // console.log(JSON.parse(result));
+
+})()
---
+++ buoi18/src/services/redis.service.js
@@ -0,0 +1,35 @@
+const Redis = require('ioredis');
+
+
+class RedisService {
+  constructor() {
+    this.redis = new Redis({
+      port: 6379, // Redis port
+      host: "127.0.0.1", // Redis host
+      db: 0
+    })
+  }
+  /**
+   *
+   * @param {String} key
+   * @param {any} value
+   * @param {int} timeout second (EX: second, PX milliseconds )
+   */
+  set(key, value, timeout) {
+    let result = null;
+    if (!timeout) {
+      result = this.redis.set(key, value); // unlimit
+    }
+    else result = this.redis.set(key, value, "EX", timeout); // expiresIn timeout(s)
+    return result;
+  }
+  get(key) {
+    const result = this.redis.get(key);
+    return result;
+  }
+  del(key) {
+    const result = this.redis.del(key);
+    return result;
+  }
+}
+module.exports = RedisService;
